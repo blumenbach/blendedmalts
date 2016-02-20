d9187fa16fef3572f8404083575c06d4790181af
Christopher Johnson <christopher.johnson@wikimedia.de>
2016-02-20 11:32:55 +0100

    removed dbpedia lookup for terms

--- scripts/highlighter.js
+++ scripts/highlighter.js
@@ -39,7 +39,7 @@ $().ready(function() {
 
     $('.tooltip').tooltipster({
         content: 'Loading...',
-	theme: 'tooltipster-light',
+	    theme: 'tooltipster-light',
         contentAsHTML: true,
         maxWidth: 1050,
         minWidth: 400,
@@ -61,13 +61,13 @@ $().ready(function() {
 
                     var cnpNo = cnp.substring(cnp.lastIndexOf('/') + 1);
 
-                    function getGND(cnpNo) {  
+                    function getGND(cnpNo) {
                         return $.ajax({
                             type: 'GET',
                             url: "/blumenbach/wisski/sites/all/themes/blendedmalts/scripts/gndCerlConvertor.php?ID=" + cnpNo,
-                            async:   false,
-                            success: function(data){
-                            return data;
+                            async: false,
+                            success: function (data) {
+                                return data;
                             }
                         }).responseText;
                     }
@@ -80,7 +80,7 @@ $().ready(function() {
                         'PREFIX wd: <http://www.wikidata.org/entity/>' +
                         'SELECT DISTINCT ?label ?desc ?s ?image WHERE { ' +
                         '?s wdt:P227 "' + GND + '" .' +
-			            '?s wdt:P18 ?image .' +
+                        '?s wdt:P18 ?image .' +
                         '?s rdfs:label ?label filter(lang(?label) = "en") .' +
                         '?s schema:description ?desc filter(lang(?desc) = "en")}').replace(/\(/g, "%28").replace(/\)/g, "%29");
                     pipeUrl = SERVICE + '?query=' + query;
@@ -99,11 +99,11 @@ $().ready(function() {
                                     var flag = true;
                                     var langFlag = true;
                                     $.each(results, function (index, value) {
-                                            flag = false;
-                                            var imgsrc = '<img src="' + results[0].image.value + '" height="90" width="70" align="right"/>';
-                                            var display = "<div>" + imgsrc + "<span STYLE='font-size: 12pt'> " + results[0].label.value + "</span><br/><span> " + results[0].desc.value + "</span></div>";
-                                            origin.tooltipster('content', display).data('ajax', 'cached');
-                                            return false;
+                                        flag = false;
+                                        var imgsrc = '<img src="' + results[0].image.value + '" height="90" width="70" align="right"/>';
+                                        var display = "<div>" + imgsrc + "<span STYLE='font-size: 12pt'> " + results[0].label.value + "</span><br/><span> " + results[0].desc.value + "</span></div>";
+                                        origin.tooltipster('content', display).data('ajax', 'cached');
+                                        return false;
                                     });
 
                                     if (flag) {
@@ -127,14 +127,14 @@ $().ready(function() {
                         'PREFIX skos: <http://www.w3.org/2004/02/skos/core#>' +
                         'PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>' +
                         'SELECT ?name ?parentString ?description WHERE {' +
-                        '?place dct:identifier "' +  code + '" .' +
+                        '?place dct:identifier "' + code + '" .' +
                         '?place xl:prefLabel ?nameLink .' +
                         '?nameLink xl:literalForm ?name .' +
                         '?place gvp:parentString ?parentString.' +
                         'OPTIONAL {?place skos:scopeNote ?note .' +
                         '?note rdf:value ?description}}').replace(/\(/g, "%28").replace(/\)/g, "%29");
 
-                    var TGNParams =    '&_implicit=false&implicit=true&_equivalent=false&_form=/sparql';
+                    var TGNParams = '&_implicit=false&implicit=true&_equivalent=false&_form=/sparql';
                     pipeUrl = SERVICE + '?query=' + query + TGNParams;
                     //pipeUrl ="http://vocab.getty.edu/sparql.json?query=PREFIX+gvp%3A+%3Chttp%3A%2F%2Fvocab.getty.edu%2Fontology%23%3E%0D%0APREFIX+dct%3A+%3Chttp%3A%2F%2Fpurl.org%2Fdc%2Felements%2F1.1%2F%3E%0D%0APREFIX+xl%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2008%2F05%2Fskos-xl%23%3E%0D%0APREFIX+skos%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2004%2F02%2Fskos%2Fcore%23%3E%0D%0APREFIX+rdf%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%3E%0D%0ASELECT+%3Fname+%3FparentString+%3Fdescription%0D%0AWHERE+%7B%0D%0A++%3Fplace+dct%3Aidentifier+%22" + code + "%22.%0D%0A++%3Fplace+xl%3AprefLabel+%3FnameLink.%0D%0A++%3FnameLink+xl%3AliteralForm+%3Fname.%0D%0A++%3Fplace+gvp%3AparentString+%3FparentString.%0D%0A++++OPTIONAL+%7B%3Fplace+skos%3AscopeNote+%3Fnote.%0D%0A%09%3Fnote+rdf%3Avalue+%3Fdescription%7D.++++%0D%0A%7D&_implicit=false&implicit=true&_equivalent=false&_form=%2Fsparql"
                     $.ajax({
@@ -163,78 +163,63 @@ $().ready(function() {
                                     if (flag) {
                                         flag = true;
                                         $.each(results, function (index, value) {
-                                        var display;
+                                            var display;
                                             var label = "<div class='form-item'><label>Label</label><ul><li> " + utf8_decode(results[index].name.value) + "</li></ul></div>";
-                                                                    var parentString = "<div class='form-item'><label>Parent</label><ul><li> (" + utf8_decode(results[index].parentString.value) + ")</li></ul></div>"
+                                            var parentString = "<div class='form-item'><label>Parent</label><ul><li> (" + utf8_decode(results[index].parentString.value) + ")</li></ul></div>"
                                             if (results[0].description) {
-                                                                        var description = "<div class='form-item'><label>Description</label><ul><li> " + utf8_decode(results[index].description.value) + "</li></ul></div>";
+                                                var description = "<div class='form-item'><label>Description</label><ul><li> " + utf8_decode(results[index].description.value) + "</li></ul></div>";
                                             } else {
-                                            description = ""
+                                                description = ""
                                             }
-                                        display = "<div class='wisski_vocab_ctrl_infobox'>" + label + parentString + description + "</div>"
-                                        origin.tooltipster('content', display).data('ajax', 'cached');
+                                            display = "<div class='wisski_vocab_ctrl_infobox'>" + label + parentString + description + "</div>"
+                                            origin.tooltipster('content', display).data('ajax', 'cached');
                                         });
                                     } else {
                                         origin.tooltipster('content', 'No Data Available');
                                     }
                                 } else {
-                                origin.tooltipster('content', 'No Data Available');
+                                    origin.tooltipster('content', 'No Data Available');
                                 }
                             }
                         }
                     });
                 } else if (tag == "TERM") {
                     var c = $(this).children().attr("ref");
+
                     function getdbpURI(text) {
                         return $.ajax({
                             type: 'GET',
                             url: "/blumenbach/wisski/sites/all/themes/blendedmalts/scripts/fetchTermData.php?term=" + text,
-                            async:   false,
-                            success: function(data){
-				console.log(data);
+                            async: false,
+                            success: function (data) {
+                                console.log(data);
                                 return data;
                             }
                         }).responseText;
                     }
-		    var text = $(this).text();	
-                    var record = $.parseJSON(getdbpURI(text)) || 0;
-		    var resource = record["link"];
-                    if (record != 0) {
-			var wpid = encodeURIComponent(resource.substring(resource.lastIndexOf('/') + 1));
-	               	var wpuri = '<https://de.wikipedia.org/wiki/' + wpid + '>';
-		        var ref = resource.indexOf('de');
-		    } else {
-		      ref = 0
-		    }	
-                    SERVICE = 'http://de.dbpedia.org/sparql';
-		    SERVICE_EN = 'http://dbpedia.org/sparql';	
-		    SERVICE_WD = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql';
 
-                    query = encodeURIComponent('PREFIX dbo: <http://dbpedia.org/ontology/>' +
-                        'SELECT ?s ?label ?abstract ?image WHERE {' +
-                        '?s rdfs:label ?label .' +
-                        '?s dbo:abstract ?abstract .' +
-                        'OPTIONAL {?s dbo:thumbnail ?image} .' +
-                        'VALUES ?s {<' + resource + '>}}').replace(/\(/g, "%28").replace(/\)/g, "%29");
-		    
+                    var text = $(this).text();
+                    var record = $.parseJSON(getdbpURI(text)) || 0;
+                    var resource = record["link"];
+                    if (record != 0) {
+                        var wpid = encodeURIComponent(resource.substring(resource.lastIndexOf('/') + 1));
+                        var wpuri = '<https://de.wikipedia.org/wiki/' + wpid + '>';
+                    }
+
+                    SERVICE_WD = 'https://query.wikidata.org/bigdata/namespace/wdq/sparql';
                     var wdquery = encodeURIComponent('PREFIX schema: <http://schema.org/>' +
                         'PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>' +
                         'PREFIX wdt: <http://www.wikidata.org/prop/direct/>' +
                         'PREFIX wd: <http://www.wikidata.org/entity/>' +
                         'SELECT ?o ?label ?desc ?image WHERE { ' +
-                         wpuri + ' schema:about ?o .' +
+                        wpuri + ' schema:about ?o .' +
                         '?o rdfs:label ?label filter(lang(?label) = "de") .' +
                         '?o schema:description ?desc filter(lang(?desc) = "en") .' +
                         'OPTIONAL {?o wdt:P18 ?image}}').replace(/\(/g, "%28").replace(/\)/g, "%29");
 
-		    if (ref > 0) {
-		    	pipeUrl = SERVICE + '?query=' + query;
-		    } else {
-		    	pipeUrl = SERVICE_EN + '?query=' + query;
-		    }
                     var wdqsUrl = SERVICE_WD + '?query=' + wdquery;
 
-		    if (c != null) {
+                    if (c != null) {
                         var res = c.split(" ");
                         var display = [];
                         for (var i = 0; i < res.length; i++) {
@@ -242,76 +227,41 @@ $().ready(function() {
                             display += importVar(res[i]);
                             display += "<hr>";
                         }
-
                         origin.tooltipster('content', display).data('ajax', 'cached');
                     } else {
                         $.ajax({
                             type: 'GET',
-                            url: pipeUrl,
+                            url: wdqsUrl,
                             dataType: 'json',
                             success: function (data) {
-				            console.log(data);
-                                    var results = data.results.bindings;
-                                    var flag = true;
-                                    if (results.length > 0) {
-                                        var lang = results[0].description;
-                                        $.each(results, function (index, value) {
-                                            var display = '<table id="ttip_content">'
-                                                + '<tr>'
-                                                + '<td>'
-                                                + '<table>'
-                                                + '<tr><td>Name:</td><td>' + results[index].label.value + '</td></tr>'
-						+ '<tr><td>Abstract:</td><td>' + results[index].abstract.value + '</td></tr>';
-                                            display += '<tr><td>URI:</td><td><a href="' + results[index].s.value + '" target="_blank">' + results[index].s.value + '</a></td></tr>';
-                                            display += '</table></td>';
-                                            if (results[index].image != undefined || results[index].image != null) {
-                                                display += '<td><div style="width:125px;height:125px;"><img style="width:125px;height:auto;" src="' + results[index].image.value + '"/></div></td>';
-                                            }
-                                            display += '</tr>';
-                                            display += '</table>';
-					 origin.tooltipster('content', display).data('ajax', 'cached');
-                                        });
-                                 } else {
-                                            origin.tooltipster('content', 'No data available for this Term');
-				}				
-			   }	
-			});
-			}
-			if (!display) {
-				 $.ajax({
-                            		type: 'GET',
-                            		url: wdqsUrl,
-                            		dataType: 'json',
-                            		success: function (data) {
-                                            console.log(data);
-                                    		var results = data.results.bindings;
-                                    		var flag = true;
-                                    		if (results.length > 0) {
-                                        		var lang = results[0].description;
-                                        		$.each(results, function (index, value) {
-                                            		var display = '<table id="ttip_content">'
-                                                	+ '<tr>'
-                                                	+ '<td>'
-                                                	+ '<table>'
-                                                	+ '<tr><td>Name:</td><td>' + results[index].label.value + '</td></tr>'
-                                                	+ '<tr><td>Abstract:</td><td>' + results[index].desc.value + '</td></tr>';
-                                            		display += '<tr><td>URI:</td><td><a href="' + results[index].o.value + '" target="_blank">' + results[index].o.value + '</a></td></tr>';
-                                            		display += '</table></td>';
-                                            		if (results[index].image != undefined || results[index].image != null) {
-                                                		display += '<td><div style="width:125px;height:125px;"><img style="width:125px;height:auto;" src="' + results[index].image.value + '"/></div></td>';
-                                            		}
-                                            		display += '</tr>';
-                                            		display += '</table>';
-                                 	        origin.tooltipster('content', display).data('ajax', 'cached');
-                            		            	});
-			                        } else {
-        	        	                    origin.tooltipster('content', 'No data available for this Term');
-                	        		}
-					}	
+                                console.log(data);
+                                var results = data.results.bindings;
+                                var flag = true;
+                                if (results.length > 0) {
+                                    var lang = results[0].description;
+                                    $.each(results, function (index, value) {
+                                        var display = '<table id="ttip_content">'
+                                            + '<tr>'
+                                            + '<td>'
+                                            + '<table>'
+                                            + '<tr><td>Name:</td><td>' + results[index].label.value + '</td></tr>'
+                                            + '<tr><td>Description:</td><td>' + results[index].desc.value + '</td></tr>';
+                                            + '<tr><td>Abstract:</td><td>' + record["abstract"] + '</td></tr>';
+                                        display += '<tr><td>URI:</td><td><a href="' + results[index].o.value + '" target="_blank">' + results[index].o.value + '</a></td></tr>';
+                                        display += '</table></td>';
+                                        if (results[index].image != undefined || results[index].image != null) {
+                                            display += '<td><div style="width:125px;height:125px;"><img style="width:125px;height:auto;" src="' + results[index].image.value + '"/></div></td>';
+                                        }
+                                        display += '</tr>';
+                                        display += '</table>';
+                                        origin.tooltipster('content', display).data('ajax', 'cached');
                                     });
-                        } else {
+                                } else {
                                     origin.tooltipster('content', 'No data available for this Term');
-                        }
+                                }
+                            }
+                        });
+                    }
                 }
             }
         }
